1e38f9d7ef63e0aa233314eddaeb5de975cd3cd2
Ilya Baskakov <baskakov.11111@mail.ru>
2021-10-09 20:17:05 +0300

    replaced secret key

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
+import bcrypt from 'bcryptjs'
+
 export default {
     MDB_URI: `mongodb://userAdmin:userAdmin123@cluster0-shard-00-00.a8f37.mongodb.net:27017,cluster0-shard-00-01.a8f37.mongodb.net:27017,cluster0-shard-00-02.a8f37.mongodb.net:27017/looona-backend?ssl=true&replicaSet=atlas-kgj87p-shard-0&authSource=admin&retryWrites=true&w=majority
     `,
-    secret: 'SECRET_KEY_RANDOM'
+    secret: bcrypt.genSaltSync(10)
 }
